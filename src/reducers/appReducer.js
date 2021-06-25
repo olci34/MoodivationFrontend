e53bf60c073921f9ded862a2c66aceadacde7690
github username <github email address>
2021-06-25 15:11:27 -0400

    add multi choice Autocomplete to WordForm

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,7 +1,6 @@
 export default function appReducer(state, action) {
   switch (action.type) {
     case "FETCH_WORDS":
-      debugger
       return { ...state, words: action.payload };
     default:
       return state;
--- src/words/WordForm.js
+++ src/words/WordForm.js
@@ -1,37 +1,35 @@
-import { useEffect, useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import { TextField, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import { useState } from "react";
+import { Button,TextField } from "@material-ui/core";
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-const useStyles = makeStyles((theme) => ({
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-  }));
 
 export default function WordForm() {
-  
-  const classes = useStyles();
 
   const [state, setState] = useState({
       word: '',
-      author: ''
+      author: '',
+      categories: []
   })
 
-  const authors = useSelector(state => [ ...new Set(state.words.map(w => w.author)) ])
+  const authors = useSelector(state => [ ...new Set(state.words.map(w => w.author).map(a => a.name)) ])
+  const categories = useSelector(state => [...new Set(state.words.map(w => w.categories).flat().map(c => c.name))])
   
   const handleChange = (e, newValue = undefined) => {
       if (newValue) {
-        setState({...state, author: newValue})
+          const name = e.target.id.split("-")[0] || "categories" // string is added because when a category removed e.target.id returns ""
+        setState({...state, [name]: newValue})
       } else {
         setState({...state, [e.target.name]: e.target.value})
       }
-      
+  }
+
+  const handleWordSubmit = (e) => {
+      e.preventDefault()
+      console.log(state)
   }
 
   return (
-    <form id="word-form">
+    <form id="word-form" onSubmit={handleWordSubmit}>
       <TextField
         name='word'
         variant="outlined"
@@ -44,14 +42,31 @@ export default function WordForm() {
         onChange={handleChange}
       />
         <Autocomplete
+          id='author'
           value={state.author}
           freeSolo
           onChange={handleChange}
-          options={authors.map(a => a.name)}
+          options={authors}
           style={{width: 300}}
-          renderInput={(params) => <TextField {...params} name='author' onChange={handleChange} label='Author' variant='outlined'/>}
+          renderInput={(params) => <TextField {...params} name='author' margin="normal" onChange={handleChange} label='Author' variant='outlined'/>}
         />
-       
+        <Autocomplete
+          id='categories'
+        //   value={state.categories}
+          multiple
+          onChange={handleChange}
+          options={categories}
+          style={{width: 300}}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              label="Categories"
+              margin="normal"
+            />
+          )}
+        />
+       <Button type="submit" variant="contained">Create Word</Button>
     </form>
   );
 }
